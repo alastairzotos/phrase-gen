7c71497987ba1e06325f7cbb506f613192635792
Alastair Zotos <alastairzotos@gmail.com>
2022-09-02 13:23:47 +0100

    feat: click back home

--- apps/web/components/app-header.tsx
+++ apps/web/components/app-header.tsx
@@ -1,4 +1,5 @@
 import { Button, PageHeader, Typography } from 'antd';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { useAuthState } from '../state/auth';
 import { getUserDetails } from '../utils/user';
@@ -8,6 +9,8 @@ import { SaveForm } from './save';
 const { Text } = Typography;
 
 export const AppHeader: React.FC = () => {
+  const { pathname, push } = useRouter();
+
   const accessToken = useAuthState(s => s.accessToken);
   const logout = useAuthState(s => s.logout);
 
@@ -32,9 +35,12 @@ export const AppHeader: React.FC = () => {
     tags = [<SaveForm key='save' />]
   }
 
+  const onBack = pathname === '/' ? undefined : () => push('/');
+
   return (
     <>
       <PageHeader
+        onBack={onBack}
         title="PhraseGen"
         avatar={{ src: '/bitmetro-logo.png', style: { backgroundColor: 'black', borderRadius: 0 } }}
         extra={extra}
