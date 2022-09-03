33a211e055fc00b8948a7db8e7669f11482b2716
Alastair Zotos <alastairzotos@gmail.com>
2022-09-03 01:40:59 +0100

    chore: remove env stuff from main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,6 @@
 import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EnvService } from './environment/environment.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +8,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1', {
     exclude: [{ path: 'health', method: RequestMethod.GET }],
   });
-
-  const env = app.get<EnvService>(EnvService).get();
 
   app.enableCors();
   
