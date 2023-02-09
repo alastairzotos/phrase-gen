ec9993bc99d2f0f7be7f2224c60d8b6e9738a853
Alastair Zotos <alastairzotos@gmail.com>
2023-02-09 21:30:11 +0700

    fix: next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: false,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   publicRuntimeConfig: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     NEXT_PUBLIC_FB_APP_ID: process.env.NEXT_PUBLIC_FB_APP_ID,
   }
 }
-
-module.exports = nextConfig
